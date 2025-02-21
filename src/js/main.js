dcf239cbeb12bc32ef56274a6c53f0f778ad9d6f
Calamych <109536910+Calamych@users.noreply.github.com>
2025-02-21 23:01:30 +0300

    Add header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import "../assets/scss/main.scss";
+
+//components
+
+import {useTheme} from "./components/theme.js";
+
+import {useBurger} from "./components/burger.js";
+
+
+useTheme();
+
+useBurger();
