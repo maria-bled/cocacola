ced1020297c0d3a83f5a473ed520cbcdc9ee6cd8
Calamych <109536910+Calamych@users.noreply.github.com>
2025-02-25 17:39:31 +0300

    Add testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,14 @@ import "../assets/scss/main.scss";
 
 //components
 
-import {useTheme} from "./components/theme.js";
-import {useInsightSlider} from "./components/slider.js"
-import {useBurger} from "./components/burger.js";
+import { useTheme } from "./components/theme.js";
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from "./components/home/slider.js";
+import { useBurger } from "./components/burger.js";
 
 useInsightSlider();
 useTheme();
-
+useTestimonialsSlider();
 useBurger();
