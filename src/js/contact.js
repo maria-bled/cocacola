2c654607c80030da28af95d53caaba0beccdf497
Calamych <109536910+Calamych@users.noreply.github.com>
2025-02-28 01:42:37 +0300

    Add contact

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+import "/scss/contact.scss";
+
+//components
+
+import { useTheme } from "./components/theme.js";
+//import { usePhone } from "./components/contact/phone.js";
+import { useBurger } from "./components/burger.js";
+
+useTheme();
+//usePhone();
+useBurger();
--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,36 @@
-import { defineConfig } from 'vite';
-import injectHTML from 'vite-plugin-html-inject';
-import { ViteMinifyPlugin } from 'vite-plugin-minify'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-
+import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-    root: './src',
+  root: "./src",
 
-    server: {
-        port: 3000,
+  server: {
+    port: 3000,
+  },
+  publicDir: "./src/assets",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "./src/index.html",
+        pricing: "./src/pricing.html",
+        contact: "./src/contact.html",
       },
-      
-      build:{
-        outDir: '../dist',
-        emptyOutDir: true,
-        rollupOptions:{
-          input:{
-main: './src/index.html',
-pricing: './src/pricing.html',
-          }
-        }
+    },
+  },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
       },
-      plugins:[
-        injectHTML(),
-        ViteMinifyPlugin(),
-        ViteImageOptimizer({
-          png: {
-            quality: 80,
-          },
-          jpg: {
-            quality: 80,
-          },
-        }),
-      ],
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
 });
